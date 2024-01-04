d4efe74b38e8ca0e19b1fd9c8c930105e06f7e3e
허준혁 <ekzmemforhs3@naver.com>
2024-01-04 12:44:41 +0900

    Add files via upload

--- Quiz1_1.java
+++ Quiz1_1.java
@@ -2,19 +2,19 @@ package quiz;
 
 import java.util.Scanner;
 
-public class Quiz1 {
+public class Quiz1_1 {
 
 	public static void main(String[] args) {
 		// 스캐너 객체 생성
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("첫 번째 숫자를 입력하세요 : ");
-		int firstNumber = sc.nextInt();  // 첫 번째 숫자 입력 받기.		
-
+		String firstNumber = sc.nextLine();  // 첫 번째 숫자 입력 받기.		
+		int f1Number = Integer.parseInt(firstNumber);
 		System.out.print("두 번째 숫자를 입력하세요 : ");
-		int secondNumber = sc.nextInt(); // 두 번째 숫자 입력 받기.		
-
-		int sum = firstNumber + secondNumber; // 두 수의 합을 변수 sum에 저장
+		String secondNumber = sc.nextLine(); // 두 번째 숫자 입력 받기.		
+		int f2Number = Integer.parseInt(secondNumber);
+		int sum = f1Number + f2Number; // 두 수의 합을 변수 sum에 저장
 		
 		System.out.println("두 수의 합 : " + sum); // 결과값 출력
 		// scanner 닫기
