7bf8d2289b5d89f511b0bcbfe326e2dc2b0fd3bb
허준혁 <ekzmemforhs3@naver.com>
2024-01-04 11:44:41 +0900

    Add files via upload

--- KeyCodeEx.java
+++ KeyCodeEx.java
@@ -5,14 +5,23 @@ public class KeyCodeEx {
 	public static void main(String[] args) throws Exception {
 		int keyCode;
 
-		keyCode = System.in.read();  //a 입력
-		System.out.println("keyCode: " + keyCode);
-		
-		keyCode = System.in.read();	 //enter 입력(CR)
-		System.out.println("keyCode: " + keyCode);
-		
-		keyCode = System.in.read();	 //enter 입력(LF)
-		System.out.println("keyCode: " + keyCode);
+		/*
+		 * keyCode = System.in.read(); //a 입력 System.out.println("keyCode: " + keyCode);
+		 * 
+		 * keyCode = System.in.read(); //enter 입력(CR) System.out.println("keyCode: " +
+		 * keyCode);
+		 * 
+		 * keyCode = System.in.read(); //enter 입력(LF) System.out.println("keyCode: " +
+		 * keyCode);
+		 */
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+			if(keyCode == 113) {
+				break;     //keyCode가 113과 동일한 경우 실행 113은 q
+			}			
+			System.out.println("종료");
+		}
 	}
 
 }
