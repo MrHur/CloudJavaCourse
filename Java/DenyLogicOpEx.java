ddbccf4bb6b4d2b4a9cbebb1125597c2cf636e38
허준혁 <ekzmemforhs3@naver.com>
2024-01-04 15:27:16 +0900

    feature

---
+++ Java/DenyLogicOpEx.java
@@ -0,0 +1,16 @@
+package sec02.exam;
+
+public class DenyLogicOpEx {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play; // false
+		System.out.println(play);
+		
+		play = !play; // true
+		System.out.println(play);
+	}
+
+}
