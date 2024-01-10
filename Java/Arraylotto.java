830c22a386a9c094a80cf3a80451010642cfe812
허준혁 <ekzmemforhs3@naver.com>
2024-01-10 10:18:20 +0900

    feature

--- Java/Arraylotto.java
+++ Java/Arraylotto.java
@@ -8,15 +8,22 @@ public class Arraylotto {
 //		해당 배열을 이용해서 배열 인덱스 0~5번까지의 값을 랜덤함수를 이용해서
 //		배열 값을 교환한 후 로또번호는 배열 0~5번째 인덱스 값으로 추출하여 나타내시오.
 
-		// 조건1. 크기가 45인 배열 선언.		
+		// 조건1. 크기가 45인 배열 선언.
 		int[] array = new int[45];
-		// 인덱스 0부터 44까지 랜덤으로 저장.		
-		for(int i = 0; i<array.length; i++) {
-			array[i] = ((int)(Math.random()*45)+1);
+		int i;
+		// 인덱스 0부터 44까지 랜덤으로 저장.
+		for (i = 0; i < array.length; i++) {
+			array[i] = ((int) (Math.random() * 45) + 1);
+			for(int j=0; j<i; j++)
+				if(array[j]==array[i]) {
+					i--;
+					break;
+				}
 		}
-		// 인덱스 0~5번까지의 로또 번호를 출력.		
-		for(int j = 0; j<=(array.length-40); j++) {
-			System.out.println("ball["+j+"] : "+array[j]); 
+		// 인덱스 0~5번까지의 로또 번호를 출력.
+		for (i = 0; i < (array.length - 39); i++) {
+			System.out.println("ball[" + i + "] : " + array[i]);
 		}
+
 	}
 }
