1dc417917bfe825a4061f133c23803dfec6fef40
허준혁 <ekzmemforhs3@naver.com>
2024-01-12 14:19:30 +0900

    feature

--- Java/Car.java
+++ Java/Car.java
@@ -1,28 +1,35 @@
-package sec03.exam;
+package sec04.exam;
 
 public class Car {
 	//필드
-	String company = "현대자동차";
-	String model;
-	String color;
-	int maxSpeed;
-
-	//생성자
-	Car() {
+	int gas;
+	
+	//생성자	
+	//메소드
+	void setGas(int gas) {
+		this.gas = gas;
 	}
 	
-	Car(String model) {
-		this(model, null, 0);
+	boolean isLeftGas() {
+		if(gas==0) {
+			System.out.println("gas가 없습니다."); 
+			return false;
+		}
+		System.out.println("gas가 있습니다."); 
+		return true;
 	}
 	
-	Car(String model, String color) {
-		this(model, color, 0);
-	}
 	
-	Car(String model, String color, int maxSpeed) {
-		this.model = model;
-		this.color = color;
-		this.maxSpeed = maxSpeed;
+	void run() {
+		while(true) {
+			if(gas > 0) {
+				System.out.println("gas잔량:" + gas + ")");
+				gas -= 1;
+			} else {
+				System.out.println("gas잔량:" + gas + ")");
+				return;
+			}
+		}
 	}
 }
 
