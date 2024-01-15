5670544ea62f0daf475a9840c2b6bccd60139294
허준혁 <ekzmemforhs3@naver.com>
2024-01-15 16:24:11 +0900

    feature

---
+++ Java/Singleton.java
@@ -0,0 +1,12 @@
+package sec05.exam;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+}
+
