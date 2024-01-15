50eb103d60f69eacb60eb19ac0a818dde51e1a21
허준혁 <ekzmemforhs3@naver.com>
2024-01-15 12:33:52 +0900

    feature

--- Java/GugudanTwoNum.java
+++ Java/GugudanTwoNum.java
@@ -9,17 +9,28 @@ public class GugudanTwoNum {
 		System.out.println("원하는 단의 숫자를 입력해주세요.");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		Gugudan_1 gugu = new Gugudan_1();
+		Gugudan_2 gugu = new Gugudan_2();
 
 		if (a >= b) {
-            for (int i = b; i <= a; i++) {
-                gugu.gugudan(i);
-            }
+			for (int i = b; i <= a; i++) {
+				gugu.gugudan(i);
+			}
 		} else {
 			for (int i = a; i <= b; i++) {
 				gugu.gugudan(i);
 			}
 
+		}
+	}
+}
+
+class Gugudan_2 {
+	void gugudan(int dan) {
+		System.out.println(dan + "단");
+		int result = 0;
+		for (int i = 1; i <= 9; i++) {
+			result = dan * i;
+			System.out.println(dan + " * " + i + " = " + result);
 		}
 	}
 }
