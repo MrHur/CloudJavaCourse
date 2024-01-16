e1db32f16949517c2e387a5cc139c89736fa16a7
허준혁 <ekzmemforhs3@naver.com>
2024-01-16 12:47:10 +0900

    feature

---
+++ Java/CharChange.java
@@ -0,0 +1,29 @@
+package quiz;
+
+public class CharChange {
+
+	public static void main(String[] args) {
+		char[] arr = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+
+		prt(arr);
+		replace(arr);
+		prt(arr);
+	}
+
+	public static void replace(char a[]) {
+		for (char i = 0; i < a.length; i++) {
+			if (a[i] == (' ')) {
+				a[i] = (',');
+			} else {
+				a[i] = a[i];
+			}
+		}
+	}
+
+	public static void prt(char a[]) {
+		for (char i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+		}
+		System.out.println();
+	}
+}
