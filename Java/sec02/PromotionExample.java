09e83db2e858dd70ba051c2ad5af2c3c2e7d5971
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-22 09:26:49 +0900

    feature

--- Java/sec02/PromotionExample.java
+++ Java/sec02/PromotionExample.java
@@ -3,8 +3,22 @@ package sec02.exam;
 public class PromotionExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		B b = new B();
+		C c = new C();
+		D d = new D();
+		E e = new E();		
+		
+		A a1 = b;
+		A a2 = c;
+		A a3 = d;
+		A a4 = e;
+		
+		B b1 = d;
+		C c1 = e;
 
+//		상속 관계에 있지 않기 때문에 컴파일 에러 발생
+		//B b3 = e;
+		//C c2 = d;
 	}
-
 }
+
