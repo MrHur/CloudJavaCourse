9c997ac2af4beccc98dfcb9874948830977bec93
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-22 11:05:17 +0900

    feature

--- Java/sec02/Casting/ChildEx.java
+++ Java/sec02/Casting/ChildEx.java
@@ -3,7 +3,7 @@ package sec02.exam.Casting;
 public class ChildEx {
 
 	public static void main(String[] args) {
-		Parent parent = new Child();
+		Parent parent = new Child(); // promotion 진행
 		
 		parent.field1 = "data1";
 		parent.method1();
@@ -13,8 +13,8 @@ public class ChildEx {
 //		parent.method3();
 		
 		Child child = (Child) parent;
-		child.field2 = "yyy"; // ����
-		child.method3(); // ����
+		child.field2 = "yyy"; // 가능
+		child.method3(); // 가능
 	}
 
 }
--- Java/sec02/Casting/InstanceofEx.java
+++ Java/sec02/Casting/InstanceofEx.java
@@ -5,15 +5,15 @@ public class InstanceofEx {
 	public static void method4(Parent parent) {
 		if(parent instanceof Child ) {
 			Child child = (Child) parent;
-			System.out.println("method4 - Child�� ��ȯ ����");
+			System.out.println("method4 - Child로 변환 성공");
 		} else {
-			System.out.println("method4 - Child�� ��ȯ���� ����");
+			System.out.println("method4 - Child로 변환되지 않음");
 		}
 	}
 	
 	public static void method5(Parent parent) {
 		Child child = (Child) parent;
-		System.out.println("method5 - Child�� ��ȯ ����");
+		System.out.println("method5 - Child로 변환 성공");
 	}
 	
 	public static void main(String[] args) {
@@ -23,7 +23,7 @@ public class InstanceofEx {
 		
 		Parent parentB = new Parent();
 		method4(parentB);
-		method5(parentB); // ���� �߻�
+		method5(parentB); // 예외 발생
 	}
 
 }
