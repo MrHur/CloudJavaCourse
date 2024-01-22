956c05c4213c9370dae2ad6d5737d9bc53e4e107
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-22 12:28:40 +0900

    feature

---
+++ Java/sec02/quiz02/Tv.java
@@ -0,0 +1,11 @@
+package quiz02;
+
+class Tv extends Product {
+	public Tv() {
+		super(420); // 슈퍼생성자 오디오 가격은 40으로 설정
+	}
+	public String toString() {
+		//toString이 없으면 반환할때 주소를 반환한다. return 문자열로 문자를 반환
+		return "Tv";
+	}
+}
