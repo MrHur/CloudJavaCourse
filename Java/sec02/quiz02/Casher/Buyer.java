d63dcac7cb9a15a26401aa7fe5197ee4cc4fbe26
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-22 12:34:13 +0900

    feature

---
+++ Java/sec02/quiz02/Casher/Buyer.java
@@ -0,0 +1,9 @@
+package quiz02.Casher;
+
+public class Buyer {
+	Product[] cart = new Product[3];
+	// 카트 바구니 .. 바구니가 다 차면 새롭게 2개의 크기만큼 생성
+	int money = 10000; // 초기 설정 돈 10000
+	int bonusPoint = 0; // 포인트는 0원
+	int i; // 물건 산 것들과 리스트를 작성하기 위해 필요한 인스턴스.
+}
