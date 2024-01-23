16461fc2219faafe8e64471dd325d1cbf06fadd0
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-23 09:32:07 +0900

    feature

---
+++ Java/chap07/sec03/PhoneEx.java
@@ -0,0 +1,14 @@
+package sec03.exam;
+
+public class PhoneEx {
+
+	public static void main(String[] args) {
+		//Phone phone = new Phone(); (x)
+		
+		SmartPhone smartPhone = new SmartPhone("홍길동");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+}
