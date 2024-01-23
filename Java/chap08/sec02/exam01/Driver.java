8e2e8bb0d03739beae0b24034480a16c97e8804f
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-23 15:14:38 +0900

    feature

---
+++ Java/chap08/sec02/exam01/Driver.java
@@ -0,0 +1,7 @@
+package sec02.exam;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ Java/chap08/sec02/exam01/Vehicle.java
@@ -0,0 +1,5 @@
+package sec02.exam;
+
+public interface Vehicle {
+	public void run();
+}
