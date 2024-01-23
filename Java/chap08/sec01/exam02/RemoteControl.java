80e9bc13a6b5345eb4cf04ead72d950e24337288
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-23 13:47:36 +0900

    feature

---
+++ Java/chap08/sec01/exam02/RemoteControl.java
@@ -0,0 +1,12 @@
+package sec01.exam02;
+
+public interface RemoteControl {
+	//필드
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	//메서드
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+}
