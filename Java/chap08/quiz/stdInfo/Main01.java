172980b1104bff50d9bbc672f4bb7697cfdc1529
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-24 12:42:50 +0900

    feature

--- Java/chap08/quiz/stdInfo/Main01.java
+++
@@ -1,9 +0,0 @@
-package quiz.stdInfo;
-
-public class Main01 {
-	public static void main(String[] args) {
-		Controller a = new Controller();
-		a.run();
-
-	}
-}
--- Java/chap08/quiz/stdInfo/Task.java
+++
@@ -1,7 +0,0 @@
-package quiz.stdInfo;
-
-public interface Task {
-	void work();
-	void test();
-
-}
