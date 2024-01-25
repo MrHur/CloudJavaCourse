ba362e0aa407cb029d6b97972db660b14647b8ef
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-25 16:25:40 +0900

    feature

---
+++ Java/chap11/src/sec01/exam/CardMain.java
@@ -0,0 +1,13 @@
+package sec01.exam;
+
+public class CardMain {
+
+	public static void main(String[] args) {
+		Card card1 = new Card();
+		Card card2 = new Card("Heart", 10);
+
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+	}
+
+}
