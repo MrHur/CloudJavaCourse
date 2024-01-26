806ef83811a10e3a29a86bc333659d817bc74d63
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-26 14:40:09 +0900

    feature

--- Java/chap12/src/sec01/exam06/ThreadNameExample.java
+++ Java/chap12/src/sec01/exam06/ThreadNameExample.java
@@ -9,6 +9,7 @@ public class ThreadNameExample {
 		System.out.println("작업 스레드 이름: " + threadA.getName());
 		threadA.start();
 		
+//		ThreadB 의 경우 setName 을 통해 이름을 설정해주지 않았으므로, 자동으로 Thread-1로 생성된 이름을 사용하게 된다.
 		ThreadB threadB = new ThreadB();
 		System.out.println("작업 스레드 이름: " + threadB.getName());
 		threadB.start();
