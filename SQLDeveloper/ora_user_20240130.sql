cff7554f40debc9a2efcc88dd0da4f098fe6c6c8
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-30 10:52:17 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240130.sql
+++ SQLDeveloper/ora_user_20240130.sql
@@ -7,6 +7,7 @@ CREATE TABLE ex2_3 (
 --SQL 또는 PL/SQL 구문에서 주석을 사용할 수 있습니다. 한 줄 전체를 주석 처리할 때는 "--" 문자를 서두에 명시하고,
 /* 여러 줄을 주석 처리할 때는  
 주석문을 사용합니다. */
+--단축키는 컨트롤 슬래쉬 
 
 SELECT column_id, column_name, data_type, data_length
 FROM user_tab_cols
@@ -21,4 +22,22 @@ TEMPORARY TABLESPACE temp;
 
 grant dba to ex_user;
 
+--날짜 데이터 타입
+CREATE TABLE EX2_5 (
+    COL_DATA   DATE,
+    COL_TIMESTAMP TIMESTAMP
+);
 
+INSERT INTO ex2_5 VALUES (SYSDATE, SYSTIMESTAMP);
+
+SELECT *
+FROM ex2_5;
+
+--NOT NULL
+CREATE TABLE ex2_6 (
+    COL_NULL        VARCHAR2(10),
+    COL_NOT_NULL    VARCHAR2(10) NOT NULL
+);
+
+--INSERT INTO ex2_6 VALUES ('AA', '');
+INSERT INTO ex2_6 VALUES ('AA', 'BB');
