7839ba780b30d9f3953392899f10730746700c0f
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-01-31 09:50:15 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240131.sql
+++ SQLDeveloper/ora_user_20240131.sql
@@ -10,3 +10,32 @@ FROM employees
 WHERE salary > 5000
 OR job_id = 'IT_PROG'
 ORDER BY employee_Id desc;
+
+--AS 생략 가능
+SELECT
+    a.employee_id,
+    a.emp_name,
+    a.department_id,
+    b.department_name AS dep_name
+FROM
+    employees   a,
+    departments b
+WHERE
+    a.department_id = b.department_id;
+    
+--VIEW 생성
+CREATE OR REPLACE VIEW emp_dept_v3 AS
+    SELECT
+        a.employee_id,
+        a.emp_name,
+        a.department_id,
+        b.department_name AS dep_name
+    FROM
+        employees   a,
+        departments b
+    WHERE
+        a.department_id = b.department_id
+--WITH CHECK OPTION;
+    WITH READ ONLY;
+    
+INSERT INTO emp_dept_v3 VALUES (2, 'AA', 2, 'asd');
