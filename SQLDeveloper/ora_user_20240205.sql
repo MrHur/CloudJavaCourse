98ca99f36b5eb7b0fc59d02b1e0b9cdf2e952acc
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-05 12:49:20 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240205.sql
+++ SQLDeveloper/ora_user_20240205.sql
@@ -121,3 +121,80 @@ SELECT period, gubun, SUM(loan_jan_amt) totl_jan
 WHERE period LIKE '2013%'
     AND region IN('서울', '경기')
 GROUP BY GROUPING SETS(period, (gubun, region));
+
+--HJH_chap05_ex1
+--사원테이블 (employees) 에서 입사년도별 사원수를 구하는 쿼리를 작성해보자.
+SELECT TO_CHAR(hire_date, 'YYYY') AS 입사년도, COUNT(*) AS 사원수
+FROM employees
+GROUP BY TO_CHAR(hire_date, 'YYYY')
+ORDER BY 입사년도;
+
+--kor_loan_status 테이블에서 2012년도 월별, 지역별 대출 총 잔액을 구하는 쿼리를 작성하라.
+SELECT period, region, SUM(loan_jan_amt) AS total_loan_amt
+FROM kor_loan_status
+WHERE period LIKE '2012%'
+GROUP BY period, region
+ORDER BY period, region;
+
+--아래의 쿼리는 분할 ROLLUP을 적용한 쿼리이다.
+ SELECT period, gubun, SUM(loan_jan_amt) totl_jan
+  FROM kor_loan_status
+WHERE period LIKE '2013%'
+GROUP BY period, ROLLUP(gubun);
+--이 쿼리를 ROLLUP을 사용하지 않고, 집합연산자를 사용해서 동일한 결과가 나오도록 쿼리를 작성해보자.
+--M1)
+SELECT period, gubun, SUM(loan_jan_amt) AS totl_jan
+FROM kor_loan_status
+WHERE period LIKE '2013%'
+GROUP BY period, gubun
+UNION ALL
+SELECT period, NULL AS gubun, SUM(loan_jan_amt) AS totl_jan
+FROM kor_loan_status
+WHERE period LIKE '2013%'
+GROUP BY period
+ORDER BY gubun;
+--M2)
+SELECT period, gubun, SUM(loan_jan_amt) AS totl_jan
+FROM kor_loan_status
+WHERE period LIKE '2013%'
+GROUP BY period, gubun
+UNION ALL
+SELECT period, '', SUM(loan_jan_amt) AS totl_jan
+FROM kor_loan_status
+WHERE period LIKE '2013%'
+GROUP BY period
+ORDER BY gubun;
+--다음 쿼리를 실행해서 결과를 확인한 후, 집합연산자를 사용해 동일한 결과를 추출하도록 쿼리를 작성해보자.
+SELECT period,
+     CASE WHEN gubun = '주택담보대출' THEN SUM(loan_jan_amt) ELSE 0 END 주택담보대출액,
+     CASE WHEN gubun = '기타대출' THEN SUM(loan_jan_amt) ELSE 0 END 기타대출액
+  FROM kor_loan_status
+WHERE period = '201311'
+GROUP BY period, gubun;
+
+SELECT period, SUM(CASE WHEN gubun = '주택담보대출' THEN loan_jan_amt ELSE 0 END) AS 주택담보대출액, 0 AS 기타대출액
+FROM kor_loan_status
+WHERE period = '201311'
+GROUP BY period
+UNION ALL
+SELECT period, 0 AS 주택담보대출액, SUM(CASE WHEN gubun = '기타대출' THEN loan_jan_amt ELSE 0 END) AS 기타대출액
+FROM kor_loan_status
+WHERE period = '201311'
+GROUP BY period
+ORDER BY period;
+
+--다음과 같은 형태, 즉 지역과 각 월별 대출총잔액을 구하는 쿼리를 작성해 보자.
+
+SELECT region,
+       TO_CHAR(SUM(CASE WHEN period = '201111' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201111",
+       TO_CHAR(SUM(CASE WHEN period = '201112' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201112",
+       TO_CHAR(SUM(CASE WHEN period = '201210' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201210",
+       TO_CHAR(SUM(CASE WHEN period = '201211' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201211",
+       TO_CHAR(SUM(CASE WHEN period = '201212' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201212",
+       TO_CHAR(SUM(CASE WHEN period = '201310' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201310",
+       TO_CHAR(SUM(CASE WHEN period = '201311' THEN loan_jan_amt ELSE 0 END), '999,999,999') AS "201311"
+FROM kor_loan_status
+GROUP BY region
+ORDER BY region;
+
+
