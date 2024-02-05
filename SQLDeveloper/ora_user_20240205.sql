ab73dce88c33868f6c29402cac5fbf96a1bf1966
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-05 17:30:57 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240205.sql
+++ SQLDeveloper/ora_user_20240205.sql
@@ -274,3 +274,102 @@ AND a.department_id = b.department_id(+);
 SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
 FROM employees a,
 departments b;
+
+--ANSI 조인(내부)
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM employees a,
+departments b
+WHERE  a.department_id = b.department_id
+AND a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM employees a
+INNER JOIN departments b
+ON (a.department_id = b.department_id)
+WHERE  a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+SELECT a.employee_id, a.emp_name, department_id, b.department_name
+FROM employees a
+INNER JOIN departments b
+USING (department_id)
+WHERE  a.hire_date >= TO_DATE('2003-01-01','YYYY-MM-DD');
+
+--ANSI 외부조인
+--외부조인
+SELECT a.employee_id, a.emp_name, job_id, b.department_id
+FROM employees a,
+    job_history b
+WHERE a.employee_id = b.employee_id (+)
+AND a.department_id = b.department_id(+);
+
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM employees a
+LEFT OUTER JOIN job_history b
+ON (a.employee_id = b.employee_id
+    and a.department_id = b.department_id);
+    
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM  job_history b
+RIGHT OUTER JOIN employees a
+ON (a.employee_id = b.employee_id
+    and a.department_id = b.department_id);
+    
+
+--CROSS 조인
+--카다시안 조인
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM employees a,
+departments b;
+
+--ANSI CROSS 조인
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM employees a
+CROSS JOIN departments b;
+
+--FULL OUTER 조인
+CREATE TABLE LEE_A (EMP_ID INT);
+CREATE TABLE LEE_B (EMP_ID INT);
+INSERT INTO LEE_A VALUES( 10);
+INSERT INTO LEE_A VALUES( 20);
+INSERT INTO LEE_A VALUES( 40);
+INSERT INTO LEE_B VALUES( 10);
+INSERT INTO LEE_B VALUES( 20);
+INSERT INTO LEE_B VALUES( 30);
+
+COMMIT;
+
+SELECT a.emp_id, b.emp_id
+FROM lee_a a,
+ lee_b b
+WHERE a.emp_id(+) = b.emp_id(+);
+
+SELECT a.emp_id, b.emp_id
+FROM lee_a a
+FULL OUTER JOIN lee_b b
+ON (a.emp_id = b.emp_id);
+
+--HJH_chap06_ex1
+--101번 사원에 대해 아래의 결과를 산출하는 쿼리를 작성해 보자.
+SELECT a.EMPLOYEE_ID 사번, a.EMP_NAME 사원명, d.JOB_title job명칭, b.start_DATE job시작일자, b.end_date job종료일자 , c.department_name job수행부서명
+FROM EMPLOYEES a,
+    job_history b,
+    departments c,
+    jobs d
+WHERE a.employee_id = b.employee_id
+and b.department_id = c.department_id
+and b.job_id = d.job_id
+and a.employee_id = 101;
+
+--아래의 쿼리를 수행하면 오류가 발생한다. 오류의 원인은 무엇인가?
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM employees a,
+     job_history b
+WHERE a.employee_id = b.employee_id(+)
+AND a.department_id(+) = b.department_id;
+--두 개의 테이블을 outer-join할 수 없습니다
+
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM employees a
+LEFT JOIN job_history b
+ON a.employee_id = b.employee_id 
+AND a.department_id = b.department_id;
