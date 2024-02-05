ad6d71f0187e7fdfed4a6c4a773175387b7eb3ca
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-05 16:32:03 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240205.sql
+++ SQLDeveloper/ora_user_20240205.sql
@@ -198,3 +198,79 @@ GROUP BY region
 ORDER BY region;
 
 
+--동등조인
+SELECT a.employee_id, a.emp_name, a.department_id, b.department_name
+FROM employees a,
+        departments b
+WHERE a.department_id = b.department_id;
+
+--세미조인
+SELECT department_id, department_name
+FROM   departments a
+WHERE EXISTS( SELECT *
+                        FROM employees b
+                        WHERE a.department_id = b.department_id
+                        AND b.salary > 3000)
+ORDER BY a.department_name;
+
+--같은 결과
+SELECT department_id, department_name
+FROM   departments a
+WHERE a.department_id IN ( SELECT b.department_id
+                        FROM employees b
+                        WHERE b.salary > 3000)
+ORDER BY department_name;
+
+--동등조인과 비교
+SELECT a.department_id, a.department_name
+FROM   departments a, employees b
+WHERE a.department_id = b.department_id
+    AND b.salary > 3000
+ORDER BY a.department_name; 
+
+--안티 조인(ANTI JOIN)
+SELECT a.employee_id, a.emp_name, a.department_id, b.department_name
+FROM employees a,
+        departments b
+WHERE a.department_id = b.department_id
+AND a.department_id NOT IN ( SELECT department_id
+                            FROM departments
+                            WHERE manager_id IS NULL);
+
+SELECT count(*)
+FROM employees a
+WHERE NOT EXISTS ( SELECT 1
+                    FROM departments c
+                    WHERE a.department_id = c.department_id
+AND manager_id IS NULL);
+
+
+--셀프 조인(SELF JOIN)
+SELECT a.employee_id, a.emp_name, b.employee_id, b.emp_name, a.department_id 
+FROM   employees a, employees b
+WHERE a.employee_id < b.employee_id
+    AND a.department_id = b.department_id
+    AND a.department_id = 20;
+    
+-- 외부조인
+-- 일반 조인과 비교
+SELECT a.department_id, a.department_name, b.job_id, b.department_id
+FROM departments a,
+    job_history b
+WHERE a.department_id = b.department_id;
+
+SELECT a.department_id, a.department_name, b.job_id, b.department_id
+FROM departments a,
+    job_history b
+WHERE a.department_id = b.department_id (+);
+
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM employees a,
+    job_history b
+WHERE a.employee_id = b.employee_id (+)
+AND a.department_id = b.department_id(+);
+
+--카타시안 조인
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM employees a,
+departments b;
