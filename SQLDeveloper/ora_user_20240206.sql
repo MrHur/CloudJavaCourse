71a68ae90b7f1f9bc09ef0de546bbe884d7a56fe
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-06 10:45:59 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240206.sql
+++ SQLDeveloper/ora_user_20240206.sql
@@ -94,4 +94,183 @@ FROM
                         where a.department_id = b.department_id
                         AND b.salary > (SELECT AVG(SALARY)
                         FROM EMPLOYEES)
-                        );   
+                        );  
+
+SELECT department_id, AVG(salary)
+    FROM employees a
+WHERE department_id IN ( SELECT department_id
+                            FROM departments
+                            WHERE parent_id = 90)
+GROUP BY department_id;  
+
+UPDATE employees a
+    SET a.salary =  (SELECT sal
+                        FROM ( SELECT b.department_id, AVG(c.salary) as sal
+                            FROM departments b,
+                                employees c
+                            WHERE b.parent_id = 90
+                                AND b.department_id = c.department_id
+                            GROUP BY b.department_id ) d
+                            WHERE a.department_id = d.department_id )
+WHERE a.department_id IN(SELECT department_id
+                                    FROM departments
+                                    WHERE parent_id = 90 );
+
+SELECT department_id, MIN(salary), MAX(salary)
+FROM employees a
+WHERE department_id IN (SELECT department_id
+                                FROM departments
+                                WHERE parent_id = 90)
+GROUP BY department_id;     
+
+MERGE INTO employees a
+USING (
+    SELECT b.department_id, AVG(c.salary) as sal
+    FROM departments b,
+         employees c
+    WHERE b.parent_id = 90
+        AND b.department_id = c.department_id
+    GROUP BY b.department_id
+) d
+ON (a.department_id = d.department_id)
+WHEN MATCHED THEN
+    UPDATE SET a.salary = d.sal;
+    
+rollback;
+
+-- 인라인뷰
+
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+  FROM employees a,
+       departments b,
+       ( SELECT AVG(c.salary) AS avg_salary 
+           FROM departments b,
+                employees c
+          WHERE b.parent_id = 90  -- 기획부
+            AND b.department_id = c.department_id ) d
+ WHERE a.department_id = b.department_id 
+   AND a.salary > d.avg_salary;
+
+
+SELECT a.* 
+  FROM ( SELECT a.sales_month, ROUND(AVG(a.amount_sold)) AS month_avg
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.sales_month BETWEEN '200001' AND '200012'
+            AND a.cust_id = b.CUST_ID
+            AND b.COUNTRY_ID = c.COUNTRY_ID
+            AND c.COUNTRY_NAME = 'Italy'     
+          GROUP BY a.sales_month  
+       )  a, --월간 매출액
+       ( SELECT ROUND(AVG(a.amount_sold)) AS year_avg
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.sales_month BETWEEN '200001' AND '200012'
+            AND a.cust_id = b.CUST_ID
+            AND b.COUNTRY_ID = c.COUNTRY_ID
+            AND c.COUNTRY_NAME = 'Italy'       
+       ) b -- 연간 매출액
+ WHERE a.month_avg > b.year_avg ;
+
+
+-- 현장 노하우
+SELECT SUBSTR(a.sales_month, 1, 4) as years,
+        a.employee_id, 
+        SUM(a.amount_sold) AS amount_sold
+   FROM sales a,
+        customers b,
+        countries c
+  WHERE a.cust_id = b.CUST_ID
+    AND b.country_id = c.COUNTRY_ID
+    AND c.country_name = 'Italy'     
+GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id;
+
+
+SELECT  years, 
+        MAX(amount_sold) AS max_sold
+ FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id, 
+                SUM(a.amount_sold) AS amount_sold
+ FROM sales a,
+               customers b,
+               countries c
+  WHERE a.cust_id = b.CUST_ID
+          AND b.country_id = c.COUNTRY_ID
+          AND c.country_name = 'Italy'     
+       GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id    
+      ) K
+ GROUP BY years
+ ORDER BY years;
+ 
+ 
+SELECT emp.years, 
+       emp.employee_id,
+       emp.amount_sold
+  FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id, 
+                SUM(a.amount_sold) AS amount_sold
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.cust_id = b.CUST_ID
+            AND b.country_id = c.COUNTRY_ID
+            AND c.country_name = 'Italy'     
+          GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id   
+        ) emp,
+       ( SELECT  years, 
+                 MAX(amount_sold) AS max_sold
+          FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id, 
+                        SUM(a.amount_sold) AS amount_sold
+                   FROM sales a,
+                        customers b,
+                        countries c
+                  WHERE a.cust_id = b.CUST_ID
+                    AND b.country_id = c.COUNTRY_ID
+                    AND c.country_name = 'Italy'     
+                  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id    
+               ) K
+          GROUP BY years
+       ) sale
+  WHERE emp.years = sale.years
+    AND emp.amount_sold = sale.max_sold 
+  ORDER BY years;
+
+
+SELECT emp.years, 
+       emp.employee_id,
+       emp2.emp_name,
+       emp.amount_sold
+  FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id, 
+                SUM(a.amount_sold) AS amount_sold
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.cust_id = b.CUST_ID
+            AND b.country_id = c.COUNTRY_ID
+            AND c.country_name = 'Italy'     
+          GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id   
+        ) emp,
+       ( SELECT  years, 
+                 MAX(amount_sold) AS max_sold
+          FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id, 
+                        SUM(a.amount_sold) AS amount_sold
+                   FROM sales a,
+                        customers b,
+                        countries c
+                  WHERE a.cust_id = b.CUST_ID
+                    AND b.country_id = c.COUNTRY_ID
+                    AND c.country_name = 'Italy'     
+                  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id    
+               ) K
+          GROUP BY years
+       ) sale,
+       employees emp2
+  WHERE emp.years = sale.years
+    AND emp.amount_sold = sale.max_sold 
+    AND emp.employee_id = emp2.employee_id
+ORDER BY years;
