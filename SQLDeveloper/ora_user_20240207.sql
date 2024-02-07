871cc386af7c2c78e40c4ed71c0c5b9f2b9df213
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-07 16:25:28 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240207.sql
+++ SQLDeveloper/ora_user_20240207.sql
@@ -41,7 +41,7 @@ BEGIN
 
   DBMS_OUTPUT.PUT_LINE( vs_emp_name || ' - ' || vs_dep_name);
 END;
-
+/
 DECLARE
   vs_emp_name employees.emp_name%TYPE;
   vs_dep_name departments.department_name%TYPE;
@@ -56,7 +56,7 @@ BEGIN
 
   DBMS_OUTPUT.PUT_LINE( vs_emp_name || ' - ' || vs_dep_name);
 END;
-
+/
 -- 현장 노하우
 CREATE TABLE ch07_varchar2 (
       VAR1 VARCHAR2(5000));
@@ -87,7 +87,7 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('SQL VARCHAR2 길이 : ' || LENGTHB(vs_sql_varchar2)); 
   DBMS_OUTPUT.PUT_LINE('PL/SQL VARCHAR2 길이 : ' || LENGTHB(vs_plsql_varchar2)); 
 END;
-
+/
 --HJH_chap07_ex1
 -- Quiz 1. 1. 구구단 중 3단을 출력하는 익명 블록을 만들어보자.
 DECLARE
@@ -107,7 +107,7 @@ BEGIN
    DBMS_OUTPUT.PUT_LINE('3 * 9 = ' || vi_num*9);
 
 END;
-
+/
 --2. 사원 테이블에서 201번 사원의 이름과 이메일주소를 출력하는 익명 블록을 만들어 보자.
 DECLARE
   vs_emp_name employees.emp_name%TYPE;
@@ -120,7 +120,7 @@ BEGIN
 
   DBMS_OUTPUT.PUT_LINE( vs_emp_name || ' - ' || vs_email);
 END;
-
+/
 --3.  사원 테이블에서 사원번호가 제일 큰 사원을 찾아낸 뒤, 이 번호 +1번으로 아래의 사원을 사원테이블에 신규 입력하는 익명 블록을 만들어 보자.
 DECLARE
     vs_max_empno employees.employee_id%TYPE;
@@ -138,7 +138,224 @@ BEGIN
     COMMIT;
     
 END;
+/
     -- 이메일이 'HARRIS'인 레코드 조회
     SELECT * 
     FROM EMPLOYEES
     WHERE EMAIL = 'HARRIS';
+
+--ORACLE-PL-SQL/09장 PLSQL문장과 서브프로그램
+-- IF 문
+
+DECLARE
+   vn_num1 NUMBER := 1;
+   vn_num2 NUMBER := 2 ;
+BEGIN
+	
+	 IF vn_num1 >= vn_num2 THEN
+	    DBMS_OUTPUT.PUT_LINE(vn_num1 ||'이 큰 수');
+	 ELSE
+	    DBMS_OUTPUT.PUT_LINE(vn_num2 ||'이 큰 수');	 
+	 END IF;
+	
+END;
+
+
+DECLARE
+   vn_salary NUMBER := 0;
+   vn_department_id NUMBER := 0;
+BEGIN
+	
+	vn_department_id := ROUND(DBMS_RANDOM.VALUE (10, 120), -1);
+  
+   SELECT salary
+     INTO vn_salary
+     FROM employees
+    WHERE department_id = vn_department_id
+      AND ROWNUM = 1;
+
+  DBMS_OUTPUT.PUT_LINE(vn_salary);
+  
+  IF vn_salary BETWEEN 1 AND 3000 THEN
+     DBMS_OUTPUT.PUT_LINE('낮음');
+  
+  ELSIF vn_salary BETWEEN 3001 AND 6000 THEN
+     DBMS_OUTPUT.PUT_LINE('중간');
+  
+  ELSIF vn_salary BETWEEN 6001 AND 10000 THEN
+     DBMS_OUTPUT.PUT_LINE('높음');
+  
+  ELSE
+     DBMS_OUTPUT.PUT_LINE('최상위');
+  END IF;  
+   
+	
+END;
+
+
+DECLARE
+   vn_salary NUMBER := 0;
+   vn_department_id NUMBER := 0;
+   vn_commission NUMBER := 0;
+BEGIN
+	
+	vn_department_id := ROUND(DBMS_RANDOM.VALUE (10, 120), -1);
+  
+   SELECT salary, commission_pct
+     INTO vn_salary, vn_commission
+     FROM employees
+    WHERE department_id = vn_department_id
+      AND ROWNUM = 1;
+
+  DBMS_OUTPUT.PUT_LINE(vn_salary);
+  
+  IF vn_commission > 0 THEN
+     IF vn_commission > 0.15 THEN
+        DBMS_OUTPUT.PUT_LINE(vn_salary * vn_commission );
+     END IF;  
+  ELSE
+     DBMS_OUTPUT.PUT_LINE(vn_salary);
+  END IF;  
+END;
+
+-- CASE 문
+DECLARE
+   vn_salary NUMBER := 0;
+   vn_department_id NUMBER := 0;
+BEGIN
+	
+	vn_department_id := ROUND(DBMS_RANDOM.VALUE (10, 120), -1);
+  
+   SELECT salary
+     INTO vn_salary
+     FROM employees
+    WHERE department_id = vn_department_id
+      AND ROWNUM = 1;
+
+  DBMS_OUTPUT.PUT_LINE(vn_salary);
+  
+  CASE WHEN vn_salary BETWEEN 1 AND 3000 THEN
+            DBMS_OUTPUT.PUT_LINE('낮음');
+       WHEN vn_salary BETWEEN 3001 AND 6000 THEN
+            DBMS_OUTPUT.PUT_LINE('중간');
+       WHEN vn_salary BETWEEN 6001 AND 10000 THEN
+            DBMS_OUTPUT.PUT_LINE('높음');
+       ELSE 
+            DBMS_OUTPUT.PUT_LINE('최상위');
+  END CASE;
+
+END;
+
+-- LOOP 문
+DECLARE
+   vn_base_num NUMBER := 3;
+   vn_cnt      NUMBER := 1;
+BEGIN
+   
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || vn_cnt || '= ' || vn_base_num * vn_cnt);
+      
+      vn_cnt := vn_cnt + 1; -- vn_cnt 값을 1씩 증가
+      
+      EXIT WHEN vn_cnt >9;  -- vn_cnt가 9보다 크면 루프 종료
+   
+   END LOOP;
+    
+END;
+
+
+-- WHILE 문
+DECLARE
+   vn_base_num NUMBER := 3;
+   vn_cnt      NUMBER := 1;
+BEGIN
+   
+   WHILE  vn_cnt <= 9 -- vn_cnt가 9보다 작거나 같을 경우에만 반복처리 
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || vn_cnt || '= ' || vn_base_num * vn_cnt);
+      
+      vn_cnt := vn_cnt + 1; -- vn_cnt 값을 1씩 증가
+      
+   END LOOP;
+    
+END;
+
+DECLARE
+   vn_base_num NUMBER := 3;
+   vn_cnt      NUMBER := 1;
+BEGIN
+   
+   WHILE  vn_cnt <= 9 -- vn_cnt가 9보다 작거나 같을 경우에만 반복처리 
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || vn_cnt || '= ' || vn_base_num * vn_cnt);
+      EXIT WHEN vn_cnt = 5;
+      vn_cnt := vn_cnt + 1; -- vn_cnt 값을 1씩 증가
+   END LOOP;    
+END;
+
+
+
+-- FOR 문
+DECLARE
+   vn_base_num NUMBER := 3;
+BEGIN
+   
+   FOR i IN 1..9 
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || i || '= ' || vn_base_num * i);
+      
+   END LOOP;
+    
+END;
+
+
+DECLARE
+   vn_base_num NUMBER := 3;
+BEGIN
+   
+   FOR i IN REVERSE 9..1
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || i || '= ' || vn_base_num * i);
+      
+   END LOOP;
+    
+END;
+
+-- CONTINUE 문
+DECLARE
+   vn_base_num NUMBER := 3;
+BEGIN
+   
+   FOR i IN 1..9 
+   LOOP
+      CONTINUE WHEN i=5;
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || i || '= ' || vn_base_num * i);
+   END LOOP;
+    
+END;
+
+-- GOTO문
+DECLARE
+   vn_base_num NUMBER := 3;
+BEGIN
+   
+   <<third>>
+   FOR i IN 1..9 
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || i || '= ' || vn_base_num * i);
+      
+      IF i = 3 THEN
+         GOTO fourth;
+      END IF;   
+   END LOOP;
+   
+   <<fourth>>
+   vn_base_num := 4;
+   FOR i IN 1..9 
+   LOOP
+      DBMS_OUTPUT.PUT_LINE (vn_base_num || '*' || i || '= ' || vn_base_num * i);
+   END LOOP;   
+    
+END;
+
+
