744d8ff15a41a2e4dd176b2ecaa945ddc79ee0fa
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-08 11:26:23 +0900

    Add files via upload

--- SQLDeveloper/ora_user_20240208.sql
+++ SQLDeveloper/ora_user_20240208.sql
@@ -51,3 +51,334 @@ BEGIN
 	 RETURN vs_country_name;  -- 국가명 반환
 	
 END;	
+
+SELECT fn_get_country_name (52777) COUN1, 
+            fn_get_country_name(10000) COUN2
+  FROM DUAL;
+  
+CREATE OR REPLACE FUNCTION fn_get_country_name ( p_country_id NUMBER )
+   RETURN VARCHAR2  -- 국가명을 반환하므로 반환 데이터타입은 VARCHAR2
+IS
+   vs_country_name COUNTRIES.COUNTRY_NAME%TYPE;
+   vn_count NUMBER := 0;
+BEGIN
+	
+	
+	SELECT COUNT(*)
+	  INTO vn_count
+	  FROM countries
+	 WHERE country_id = p_country_id;
+	 
+  IF vn_count = 0 THEN
+     vs_country_name := '해당국가 없음';
+  ELSE
+	
+	  SELECT country_name
+	    INTO vs_country_name
+	    FROM countries
+	   WHERE country_id = p_country_id;
+	      
+  END IF;
+	 
+ RETURN vs_country_name;  -- 국가명 반환
+	
+END;	  
+
+SELECT fn_get_country_name (52777) COUN1,
+            fn_get_country_name(10000) COUN2
+FROM DUAL;
+
+
+CREATE OR REPLACE FUNCTION fn_get_user
+   RETURN VARCHAR2  -- 반환 데이터타입은 VARCHAR2
+IS
+   vs_user_name VARCHAR2(80);
+BEGIN
+	SELECT USER
+    INTO vs_user_name
+    FROM DUAL;
+		 
+  RETURN vs_user_name;  -- 사용자이름 반환
+	
+END;	 	
+
+SELECT fn_get_user(),fn_get_user
+FROM DUAL;
+
+-- 프로시저 생성
+
+CREATE OR REPLACE PROCEDURE my_new_job_proc 
+          ( p_job_id    IN JOBS.JOB_ID%TYPE,
+            p_job_title IN JOBS.JOB_TITLE%TYPE,
+            p_min_sal   IN JOBS.MIN_SALARY%TYPE,
+            p_max_sal   IN JOBS.MAX_SALARY%TYPE )
+IS
+
+BEGIN
+	
+	INSERT INTO JOBS ( job_id, job_title, min_salary, max_salary, create_date, update_date)
+	          VALUES ( p_job_id, p_job_title, p_min_sal, p_max_sal, SYSDATE, SYSDATE);
+	          
+	COMMIT;
+	
+	
+END ;            
+
+-- 프로시저 실행
+
+EXEC my_new_job_proc ('SM_JOB1', 'Sample JOB1', 1000, 5000);
+
+SELECT *
+  FROM jobs
+ WHERE job_id = 'SM_JOB1';
+ 
+EXEC my_new_job_proc ('SM_JOB1', 'Sample JOB1', 1000, 5000); 
+ 
+ 
+CREATE OR REPLACE PROCEDURE my_new_job_proc 
+          ( p_job_id    IN JOBS.JOB_ID%TYPE,
+            p_job_title IN JOBS.JOB_TITLE%TYPE,
+            p_min_sal   IN JOBS.MIN_SALARY%TYPE,
+            p_max_sal   IN JOBS.MAX_SALARY%TYPE )
+IS
+  vn_cnt NUMBER := 0;
+BEGIN
+	
+	-- 동일한 job_id가 있는지 체크
+	SELECT COUNT(*)
+	  INTO vn_cnt
+	  FROM JOBS
+	 WHERE job_id = p_job_id;
+	 
+	-- 없으면 INSERT 
+	IF vn_cnt = 0 THEN 
+	
+	   INSERT INTO JOBS ( job_id, job_title, min_salary, max_salary, create_date, update_date)
+	             VALUES ( p_job_id, p_job_title, p_min_sal, p_max_sal, SYSDATE, SYSDATE);
+	             
+	ELSE -- 있으면 UPDATE
+	
+	   UPDATE JOBS
+	      SET job_title   = p_job_title,
+	          min_salary  = p_min_sal,
+	          max_salary  = p_max_sal,
+	          update_date = SYSDATE
+	    WHERE job_id = p_job_id;
+	
+  END IF;
+	          
+	COMMIT;
+	
+	
+END ;   
+
+EXEC my_new_job_proc ('SM_JOB1', 'Sample JOB1', 2000, 6000);
+
+EXEC my_new_job_proc ('SM_JOB2', 'Sample JOB2', 3000, 9000);
+
+SELECT *
+  FROM jobs
+ WHERE job_id = 'SM_JOB1';
+ 
+ 
+-- 매개변수 디폴트 값 설정 
+ 
+EXEC my_new_job_proc ('SM_JOB1', 'Sample JOB1'); 
+ 
+ 
+CREATE OR REPLACE PROCEDURE my_new_job_proc 
+          ( p_job_id    IN JOBS.JOB_ID%TYPE,
+            p_job_title IN JOBS.JOB_TITLE%TYPE,
+            p_min_sal   IN JOBS.MIN_SALARY%TYPE := 10,
+            p_max_sal   IN JOBS.MAX_SALARY%TYPE := 100 )
+IS
+  vn_cnt NUMBER := 0;
+BEGIN
+	
+	-- 동일한 job_id가 있는지 체크
+	SELECT COUNT(*)
+	  INTO vn_cnt
+	  FROM JOBS
+	 WHERE job_id = p_job_id;
+	 
+	-- 없으면 INSERT 
+	IF vn_cnt = 0 THEN 
+	
+	   INSERT INTO JOBS ( job_id, job_title, min_salary, max_salary, create_date, update_date)
+	             VALUES ( p_job_id, p_job_title, p_min_sal, p_max_sal, SYSDATE, SYSDATE);
+	             
+	ELSE -- 있으면 UPDATE
+	
+	   UPDATE JOBS
+	      SET job_title   = p_job_title,
+	          min_salary  = p_min_sal,
+	          max_salary  = p_max_sal,
+	          update_date = SYSDATE
+	    WHERE job_id = p_job_id;
+	
+  END IF;
+	          
+	COMMIT;
+	
+	
+END ;    
+
+
+EXECUTE my_new_job_proc ('SM_JOB1', 'Sample JOB1');
+
+SELECT *
+  FROM jobs
+ WHERE job_id = 'SM_JOB1';
+ 
+-- OUT, IN OUT 매개변수 사용
+
+CREATE OR REPLACE PROCEDURE my_new_job_proc 
+          ( p_job_id    IN  JOBS.JOB_ID%TYPE,
+            p_job_title IN  JOBS.JOB_TITLE%TYPE,
+            p_min_sal   IN  JOBS.MIN_SALARY%TYPE := 10,
+            p_max_sal   IN  JOBS.MAX_SALARY%TYPE := 100,
+            p_upd_date  OUT JOBS.UPDATE_DATE%TYPE  )
+IS
+  vn_cnt NUMBER := 0;
+  vn_cur_date JOBS.UPDATE_DATE%TYPE := SYSDATE;
+BEGIN
+	
+	-- 동일한 job_id가 있는지 체크
+	SELECT COUNT(*)
+	  INTO vn_cnt
+	  FROM JOBS
+	 WHERE job_id = p_job_id;
+	 
+	-- 없으면 INSERT 
+	IF vn_cnt = 0 THEN 
+	
+	   INSERT INTO JOBS ( job_id, job_title, min_salary, max_salary, create_date, update_date)
+	             VALUES ( p_job_id, p_job_title, p_min_sal, p_max_sal, vn_cur_date, vn_cur_date);
+	             
+	ELSE -- 있으면 UPDATE
+	
+	   UPDATE JOBS
+	      SET job_title   = p_job_title,
+	          min_salary  = p_min_sal,
+	          max_salary  = p_max_sal,
+	          update_date = vn_cur_date
+	    WHERE job_id = p_job_id;
+	
+  END IF;
+  
+	-- OUT 매개변수에 일자 할당
+	p_upd_date := vn_cur_date;
+	          
+	COMMIT;
+	
+	
+END ;   
+
+
+DECLARE
+   vd_cur_date JOBS.UPDATE_DATE%TYPE;
+BEGIN
+	 my_new_job_proc ('SM_JOB1', 'Sample JOB1', 2000, 6000, vd_cur_date);
+	 
+	 DBMS_OUTPUT.PUT_LINE(vd_cur_date);
+END;
+
+
+CREATE OR REPLACE PROCEDURE my_parameter_test_proc (
+               p_var1        VARCHAR2,
+               p_var2 OUT    VARCHAR2,
+               p_var3 IN OUT VARCHAR2 )
+IS
+
+BEGIN
+	 DBMS_OUTPUT.PUT_LINE('p_var1 value = ' || p_var1);
+	 DBMS_OUTPUT.PUT_LINE('p_var2 value = ' || p_var2);
+	 DBMS_OUTPUT.PUT_LINE('p_var3 value = ' || p_var3);
+	 
+	 p_var2 := 'B2';
+	 p_var3 := 'C2';
+	
+END;               
+
+
+DECLARE 
+   v_var1 VARCHAR2(10) := 'A';
+   v_var2 VARCHAR2(10) := 'B';
+   v_var3 VARCHAR2(10) := 'C';
+BEGIN
+	 my_parameter_test_proc (v_var1, v_var2, v_var3);
+	 
+	 DBMS_OUTPUT.PUT_LINE('v_var2 value = ' || v_var2);
+	 DBMS_OUTPUT.PUT_LINE('v_var3 value = ' || v_var3);
+END;
+
+
+-- RETURN 문
+CREATE OR REPLACE PROCEDURE my_new_job_proc 
+          ( p_job_id    IN  JOBS.JOB_ID%TYPE,
+            p_job_title IN  JOBS.JOB_TITLE%TYPE,
+            p_min_sal   IN  JOBS.MIN_SALARY%TYPE := 10,
+            p_max_sal   IN  JOBS.MAX_SALARY%TYPE := 100
+            --p_upd_date  OUT JOBS.UPDATE_DATE%TYPE  
+            )
+IS
+  vn_cnt NUMBER := 0;
+  vn_cur_date JOBS.UPDATE_DATE%TYPE := SYSDATE;
+BEGIN
+	-- 1000 보다 작으면 메시지 출력 후 빠져나간다
+	IF p_min_sal < 1000 THEN
+	   DBMS_OUTPUT.PUT_LINE('최소 급여값은 1000 이상이어야 합니다');
+	   RETURN;
+  END IF;
+	
+	-- 동일한 job_id가 있는지 체크
+	SELECT COUNT(*)
+	  INTO vn_cnt
+	  FROM JOBS
+	 WHERE job_id = p_job_id;
+	 
+	-- 없으면 INSERT 
+	IF vn_cnt = 0 THEN 
+	
+	   INSERT INTO JOBS ( job_id, job_title, min_salary, max_salary, create_date, update_date)
+	             VALUES ( p_job_id, p_job_title, p_min_sal, p_max_sal, vn_cur_date, vn_cur_date);
+	             
+	ELSE -- 있으면 UPDATE
+	
+	   UPDATE JOBS
+	      SET job_title   = p_job_title,
+	          min_salary  = p_min_sal,
+	          max_salary  = p_max_sal,
+	          update_date = vn_cur_date
+	    WHERE job_id = p_job_id;
+	
+  END IF;
+  
+	          
+	COMMIT;
+	
+	
+END ;   
+
+
+EXEC my_new_job_proc ('SM_JOB1', 'Sample JOB1', 999, 6000);
+
+
+--- 현장 노하우 1
+DECLARE
+  emp_name VARCHAR2(80);
+BEGIN
+	emp_name := 'hong';
+	
+	UPDATE employees
+	   SET emp_name = emp_name
+	 WHERE employee_id = 100;
+	 
+	SELECT emp_name
+	  INTO emp_name
+	  FROM employees
+	 WHERE employee_id = 100;
+	 
+	DBMS_OUTPUT.PUT_LINE(emp_name);
+	
+END;
