b3e4a6189a51f1239b552ec4c561c4a99f818f60
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-14 09:50:15 +0900

    feature

---
+++ JSP/JspWeb/main/java/test/testServlet.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/Test")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("testServlet!!");
+		response.setContentType("text/html; charset=UTF-8"); // 인코딩 설정
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Test</title></head>");
+		out.print("<body><h1>좋은 하루!!</h1><h2>화이팅~^^</h2></body>");
+		out.print("</html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
