edac91988bfc8ea9e54fdce9a9e2ffdd5337d5ee
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-16 16:10:21 +0900

    feature

---
+++ JSP/JspWeb/main/java/exercise/SelectServlet.java
@@ -0,0 +1,62 @@
+package exercise;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/SelectServlet")
+public class SelectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SelectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		String job = request.getParameter("job");
+		String interests[] = request.getParameterValues("interest");
+
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("당신이 선택한 직업 : ");
+		// 향상된 for 문 사용.
+		out.print("<b>");
+		out.println(job);
+		out.print("</b>");
+		out.println("<hr>");
+		out.print("당신이 선택한 관심 분야 : ");
+		for(int i = 0; i<interests.length; i++) {
+			out.println("<b>"+interests[i]+"</b>" + " ");
+		}
+		//자바스크립트로 이전 페이지로 이동하는 링크를 만들어 줌
+		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
