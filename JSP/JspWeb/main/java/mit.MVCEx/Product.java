0537bea3a7accd8b539064971e816cab8dca93ae
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-22 10:44:37 +0900

    Add files via upload

--- JSP/JspWeb/main/java/mit.MVCEx/Product.java
+++ JSP/JspWeb/main/java/mit.MVCEx/Product.java
@@ -1,14 +1,12 @@
 package mit.MVCEx;
 
 public class Product {
-	//필드
 	private String id;
 	private String name;
 	private String maker;
 	private int price;
 	private String date;
-	
-	//생성자
+
 	public Product(String id, String name, String maker, int price, String date) {
 		this.id = id;
 		this.name = name;
@@ -16,35 +14,43 @@ public class Product {
 		this.price = price;
 		this.date = date;
 	}
-	
-	// getter setter 메서드
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMaker() {
 		return maker;
 	}
+
 	public void setMaker(String maker) {
 		this.maker = maker;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
