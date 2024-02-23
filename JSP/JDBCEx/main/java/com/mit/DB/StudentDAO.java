34ae8e0083b81991425b43294b2f115db405178c
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-02-23 11:12:36 +0900

    Add files via upload

--- JSP/JDBCEx/main/java/com/mit/DB/StudentDAO.java
+++ JSP/JDBCEx/main/java/com/mit/DB/StudentDAO.java
@@ -3,35 +3,84 @@ package com.mit.DB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentDAO {
 	Connection conn = null;
 	PreparedStatement pstmt;
-	
+
 	/*
 	 * Connection conn = DriverManager.getConnection(
 	 * 'jdbc:oracle:thin:@localhost:1521:xe', 'ora_uesr', '1234');
 	 */
-	
+
 	final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	
+
 	public void open() {
 		try {
 			Class.forName(JDBC_DRIVER);
-			conn = DriverManager.getConnection(JDBC_URL, "ora_user","1234");			
+			conn = DriverManager.getConnection(JDBC_URL, "ora_user", "1234");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void close() {
 		try {
 			pstmt.close();
-			conn.close();			
+			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void insert(Student s) {
+		open();
+		String sql = "INSERT INTO student(id, username, univ, birth, email) values(SEQ_ID.NEXTVAL,?,?,?,?)";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, s.getUsername());
+			pstmt.setString(2, s.getUniv());
+			pstmt.setDate(3, s.getBirth());
+			pstmt.setString(4, s.getEmail());
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	public List<Student> getAll() {
+		open();
+		List<Student> students = new ArrayList<>();
+
+		try {
+			pstmt = conn.prepareStatement("Select * from student order by 1");
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Student s = new Student();
+				s.setId(rs.getInt("id"));
+				s.setUsername(rs.getString("username"));
+				s.setUniv(rs.getString("univ"));
+				s.setBirth(rs.getDate("birth"));
+				s.setEmail(rs.getString("email"));
+
+				students.add(s);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			close();
+		}
+		return students;
 	}
 }
