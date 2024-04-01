a3df32f5e678ef11c8e8bad3864f525bb37554ee
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-04-01 14:04:52 +0900

    Add files via upload

---
+++ Python/list01.py
@@ -0,0 +1,26 @@
+# 출력합니다
+
+print("# 리스트")
+list_a = [1, 2, 3]
+list_b = [4, 5, 6]
+
+print("list_a = ", list_a)
+print("list_b = ", list_b)
+print()
+
+# 기본 연산자
+print("# 리스트 기본 연산자")
+print("list_a + list_b =", list_a + list_b)
+print("list_a * 3 =", list_a * 3)
+print()
+
+#함수
+print("# 길이 구하기")
+print("len(list_a) = ", len(list_a))
+
+cities = ['서울', '부산', '인천', '대구', '대전', '광주', '울산', '수원']
+cities[0:6]
+cities[5:]
+cities[-8:]
+cities[:]
+cities[-50:50]
