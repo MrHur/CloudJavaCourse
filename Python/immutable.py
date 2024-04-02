8ff2018ea6c783818bd909bb2c1f0f2d53bf47db
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-04-02 11:56:58 +0900

    Add files via upload

---
+++ Python/immutable.py
@@ -0,0 +1,10 @@
+def f(i, mylist):
+  i = i + 1
+  mylist.append(0)
+  print(i, mylist)
+
+k = 10         # k는 int (immutable)
+m = [1,2,3]    # m은 리스트 (mutable)
+
+f(k, m)        # 함수 호출
+print(k, m)    # 호출자 값 체크
