01d108d082194306cc1552b8ab374b8da81d89b8
Greedy92 <79696786+MrHur@users.noreply.github.com>
2024-04-03 09:52:22 +0900

    Add files via upload

--- Python/yesterday.py
+++ Python/yesterday.py
@@ -8,3 +8,10 @@ for line in yesterday_lyric:
 
 n_of_yesterday = contents.upper().count("YESTERDAY")
 print("Number of a Word 'Yesterday'" , n_of_yesterday)
+
+
+# 파일을 읽기 모드로 열기
+with open("yesterday.txt", "r") as file:
+    # 파일 내용을 읽어와서 출력
+    contents = file.read()
+    print(contents)
